f0e3d2731652e7c964012218db5a8034221521bd
wangrui06 <wangrui06@shengpay.com>
2023-05-04 10:25:43 +0800

    first commit

--- src/main/java/st/coo/memo/common/MyBatisFlexConfig.java
+++ src/main/java/st/coo/memo/common/MyBatisFlexConfig.java
@@ -3,18 +3,24 @@ package st.coo.memo.common;
 import com.mybatisflex.core.audit.AuditManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 @Configuration
 public class MyBatisFlexConfig {
 
-    public MyBatisFlexConfig() {
-        //开启审计功能
-        AuditManager.setAuditEnable(true);
+    @Value("${enable.sql.log:false}")
+    private boolean enableSqlLog;
 
-        //设置 SQL 审计收集器
-        AuditManager.setMessageCollector(auditMessage ->
-                log.info("{},{}ms,{}", auditMessage.getFullSql(), auditMessage.getElapsedTime(),auditMessage.getQueryCount())
-        );
+    public MyBatisFlexConfig() {
+        if (enableSqlLog) {
+            //开启审计功能
+            AuditManager.setAuditEnable(true);
+
+            //设置 SQL 审计收集器
+            AuditManager.setMessageCollector(auditMessage ->
+                    log.info("{},{}ms,{}", auditMessage.getFullSql(), auditMessage.getElapsedTime(), auditMessage.getQueryCount())
+            );
+        }
     }
 }
